f3eaefbcc235f6e739dd812a7d77089449b15718
FABIO SOUSA <fabio.eletrotec@hotmail.com>
2023-10-08 16:39:33 -0300

    Plano Operadora versão02

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,8 +1,23 @@
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.Calendar;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
+
+        Calendar relogio = Calendar.getInstance();
+        int hora = relogio.get(Calendar.HOUR_OF_DAY);
+
+        if (hora > 6 && hora < 12) {
+            System.out.println("Bom Dia");
+        } else if (hora > 12 && hora < 18) {
+            System.out.println("Boa Tarde");
+        } else {
+            System.out.println("Boa Noite");
+        }
+
+        System.out.println("Data/Hora atual: "+ relogio.getTime());
+
         try (Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
 
             int numero = 1021;
@@ -11,18 +26,17 @@ public class ContaTerminal {
             System.out.println("Por favor, digite seu nome completo: ");
             String nomeCliente = scanner.next();
 
-
             System.out.println("Por favor, digite o número da Agência: ");
             String agencia = scanner.next();
 
             System.out.println("Por favor, digite o valor do deposito inicial: ");
             saldo = scanner.nextDouble();
-            
+
             System.out.println("Olá " + nomeCliente + ", ");
             System.out.println("obrigado por abrir uma conta em nosso banco, ");
-            System.out.println("sua agência: " + agencia +" / conta n°: " + numero + " e ");
+            System.out.println("sua agência: " + agencia + " / conta n°: " + numero + " e ");
             System.out.println("seu saldo: R$ " + saldo + ", já está disponível para saque. ");
         }
-        
+
     }
 }
--- ContaBanco/src/PlanoOperadora.java
+++ ContaBanco/src/PlanoOperadora.java
@@ -1,28 +1,71 @@
+
+//import java.util.Locale;
+import java.util.Scanner;
+
 public class PlanoOperadora {
+    
+
+    public static void criarConta() {
+
+        String basic = "100 minutos de ligação";
+        String midia = "";
+        String turbo = "";
+        String nomePlano = "";
+        String plano = "";
+
+        Scanner entrada = new Scanner(System.in);
+
+        if (entrada.hasNextLine()) {
+            plano = entrada.next();
+        } else {
+            System.out.println("ERRO!!!");
+        }
+
+      //  nomePlano = (plano == "B") ? "BASIC: "
+       //         : (plano == "M") ? "MIDIA: " : (plano == "T") ? "TURBO: " : "Escolha B, M ou T.";
+      //  System.out.println(nomePlano);
+
+        switch (plano) {
+            case "T": {
+                nomePlano = "TURBO: ";
+                break;
+            }
+            case "M": {
+                nomePlano = "MIDIA ";
+                break;
+            }
+            case "B": {
+                nomePlano = "BASIC ";
+                break;
+            }
+            default: {
+                nomePlano = "Escolha B, M ou T.";
+            }
+        }
+
+        switch (plano) {
+            case "T": {
+                turbo = " + 5Gb Youtube";
+            }
+            case "M": {
+                midia = " + Whats e Intagram grátis";
+            }
+            case "B": {
+                System.out.println(nomePlano + basic + midia + turbo + ".");
+                entrada.close();
+                break;
+            }
+            default: {
+                System.out.println("Valor Inválido " + nomePlano);
+                criarConta();
+            }
+        }
+
+    }
+
     public static void main(String[] args) throws Exception {
 
-      String basic = "100 minutos de ligação";
-      String midia = "";
-      String turbo = "";
-
-      String plano = "T"; // M / T
-      String nomePlano = plano == "B" ? "BASIC: " : plano == "M" ? "MIDIA: " : plano == "T" ? "TURBO: " : "Escolha B, M ou T.";
-
-		switch (plano) {
-			case "T": {
-				turbo = " + 5Gb Youtube";
-			}
-			case "M": {
-				midia = " + Whats e Intagram grátis";
-			}
-			case "B": {
-				System.out.println(nomePlano + basic + midia + turbo + ".");
-                break;
-			}
-            default: {
-                System.out.println("Plano Inválido " + nomePlano);
-            }
-		}
-
+        System.out.println("Digite B, M ou T e conheça um dos planos.");
+        criarConta();
     }
 }
